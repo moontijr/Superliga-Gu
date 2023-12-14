cf43db836e245ab598c3f31e06cf0bc5e710275b
moontijr <munteantudor03@yahoo.com>
2023-12-15 01:23:34 +0200

    LEADERBOARD page facut, doar ca nu arata inca cum ar trebui

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,12 +2,38 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private ImageView button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+        logoImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLeaderboardActivity();
+            }
+        });
+    }
+
+    public void openLeaderboardActivity(){
+        Intent intent= new Intent(this, LeaderboardActivity.class);
+        startActivity(intent);
+    }
+
+    public void onLogoClick(View view) {
+        // Start the new activity
+        button = findViewById(R.id.logoImageView);
+        button.setOnClickListener(v -> openLeaderboardActivity());
     }
 }
