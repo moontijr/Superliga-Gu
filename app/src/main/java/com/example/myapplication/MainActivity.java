ae35b984e6bebbc8c2a8a0bf5c0e879771c9a1c6
Vlad <vlad.pasca17@gmail.com>
2023-12-15 22:31:00 +0200

    -Cleaned MainActivity -Register + Extra details in DB Insert

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -19,46 +27,65 @@ import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
-
     private DatabaseReference mDatabase;
 
-    Dialog popUpDialog;
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Firebase initialization
         Log.d("Firebase", "Initializing Firebase");
         FirebaseApp.initializeApp(this);
-        Log.d("Firebase", "Firebase initialized succesfully");
+        Log.d("Firebase", "Firebase initialized successfully");
 
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
         mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
+        if (mDatabase == null) {
+            Log.e("Database", "Database reference is null");
+        } else {
+            Log.d("Database", "Database reference initialized successfully");
+        }
 
-        Button registerButton = findViewById(R.id.registerButton);
+        firebaseAuth = FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MatchdaysActivity.class));
+            finish();
+        }
 
-        popUpDialog = new Dialog(this);
 
-        registerButton.setOnClickListener(v -> {
-            if (popUpDialog != null && popUpDialog.isShowing()) {
-                // Dismiss the existing dialog
-                popUpDialog.dismiss();
+        //Register dialog button
+        Button registerPopupButton = findViewById(R.id.registerPopupButton);
+        registerPopupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRegisterPopupDialog();
             }
+        });
+    }
 
-            // Inflate the popup layout
-            View popupView = getLayoutInflater().inflate(R.layout.register_popup, null);
+    //Register Dialog
+    private void showRegisterPopupDialog(){
+        Dialog popupDialog = new Dialog(this);
 
-            // Find the EditText views in the popup view
-            EditText editTextUsername = popupView.findViewById(R.id.Username);
-            EditText editTextPassword = popupView.findViewById(R.id.Password);
-            EditText editTextFamilyName = popupView.findViewById(R.id.NumeFamilie);
-            EditText editTextGivenName = popupView.findViewById(R.id.Prenume);
-            EditText editTextMailAddress = popupView.findViewById(R.id.Email);
+        popupDialog.setContentView(R.layout.register_popup);
 
-            // Set up a button in the popup layout to save the user data
-            Button saveButton = popupView.findViewById(R.id.registerAction);
-            saveButton.setOnClickListener(q -> {
+        // Find the EditText views in the popup view
+        EditText editTextUsername = popupDialog.findViewById(R.id.Username);
+        EditText editTextPassword = popupDialog.findViewById(R.id.Password);
+        EditText editTextFamilyName = popupDialog.findViewById(R.id.NumeFamilie);
+        EditText editTextGivenName = popupDialog.findViewById(R.id.Prenume);
+        EditText editTextMailAddress = popupDialog.findViewById(R.id.Email);
+        Button registerAction = popupDialog.findViewById(R.id.registerAction);
+
+        //Set up the register action
+        registerAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 // Get values from the EditText fields
                 String username = editTextUsername.getText().toString();
                 String password = editTextPassword.getText().toString();
@@ -66,115 +93,43 @@ public class MainActivity extends AppCompatActivity {
                 String givenName = editTextGivenName.getText().toString();
                 String email = editTextMailAddress.getText().toString();
 
-//                // Call the method to add the user to the database
-//                addUserToDb(username, password, familyName, givenName, email);
+                if (TextUtils.isEmpty(email)) {
+                    // Show an error message or handle the invalid email case
+                    editTextMailAddress.setError("Enter a valid email address");
+                    return;  // Stop further processing if the email is invalid
+                }
 
-                //TODO register pt Auth
+                if(TextUtils.isEmpty(password)){
+                    editTextPassword.setError("Enter a password");
+                    return;
+                }
 
-                // Dismiss the popup after saving
-                popUpDialog.dismiss();
-            });
+                //Register the user in Firebase
+                firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "New user registered.",Toast.LENGTH_SHORT).show();
 
-            // Set the content view of the popup dialog
-            popUpDialog.setContentView(popupView);
+                            // Call the method to add the extra user details to the database
+                            addUserToDb(username, password, familyName, givenName, email);
 
-            // Make the dialog background transparent
-            Objects.requireNonNull(popUpDialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                            startActivity(new Intent(getApplicationContext(), MatchdaysActivity.class));
+                    }else {
+                            Toast.makeText(MainActivity.this,"Error! "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
-            // Show the popup dialog
-            popUpDialog.show();
-        });
 
-        Button loginButton = (Button) findViewById(R.id.loginButton);
 
-        popUpDialog = new Dialog(this);
-
-        loginButton.setOnClickListener(v -> {
-            if (popUpDialog != null && popUpDialog.isShowing()) {
-                // Dismiss the existing dialog
-                popUpDialog.dismiss();
+                popupDialog.dismiss();
             }
-
-            // Inflate the popup layout
-            View popupView = getLayoutInflater().inflate(R.layout.login_popup, null);
-
-            // Find the EditText views in the popup view
-            EditText editTextMailAddress = popupView.findViewById(R.id.loginEmail);
-            EditText editTextPassword = popupView.findViewById(R.id.loginPassword);
-
-            // Set up a button in the popup layout to save the user data
-            Button saveButton = popupView.findViewById(R.id.loginAction);
-            saveButton.setOnClickListener(q -> {
-                // Get values from the EditText fields
-                String email = editTextMailAddress.getText().toString();
-                String password = editTextPassword.getText().toString();
-
-                //TODO login FBA
-
-                // Show the popup dialog
-                popUpDialog.show();
-            });
-            // Set the content view of the popup dialog
-            popUpDialog.setContentView(popupView);
-
-            // Make the dialog background transparent
-            Objects.requireNonNull(popUpDialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-
-            // Show the popup dialog
-            popUpDialog.show();
-
         });
 
-        if (mDatabase == null) {
-            Log.e("Database", "Database reference is null");
-        } else {
-            Log.d("Database", "Database reference initialized successfully");
-        }
+        popupDialog.show();
     }
 
-    public void onRegisterButtonClick(View view) {
-
-        if (popUpDialog != null && popUpDialog.isShowing()) {
-            // Dismiss the existing dialog
-            popUpDialog.dismiss();
-        }
-
-        // Inflate the popup layout
-        View popupView = getLayoutInflater().inflate(R.layout.register_popup, null);
-
-        // Find the EditText views in the popup view
-        EditText editTextUsername = popupView.findViewById(R.id.Username);
-        EditText editTextParola = popupView.findViewById(R.id.Password);
-        EditText editTextFamilyName = popupView.findViewById(R.id.NumeFamilie);
-        EditText editTextGivenName = popupView.findViewById(R.id.Prenume);
-        EditText editTextMailAdress = popupView.findViewById(R.id.Email);
-
-        // Set up a button in the popup layout to save the user data
-        Button saveButton = popupView.findViewById(R.id.registerAction);
-        saveButton.setOnClickListener(v -> {
-            // Get values from the EditText fields
-            String username = editTextUsername.getText().toString();
-            String password = editTextParola.getText().toString();
-            String familyName = editTextFamilyName.getText().toString();
-            String givenName = editTextGivenName.getText().toString();
-            String email = editTextMailAdress.getText().toString();
-
-            // Call the method to add the user to the database
-            addUserToDb(username, password, familyName, givenName, email);
-
-            // Dismiss the popup after saving
-            popUpDialog.dismiss();
-        });
-
-        // Set the content view of the popup dialog
-        popUpDialog.setContentView(popupView);
-
-        // Make the dialog background transparent
-        Objects.requireNonNull(popUpDialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-
-        // Show the popup dialog
-        popUpDialog.show();
-    }
 
     private void addUserToDb(String username, String password, String familyName, String givenName, String email) {
         User user = new User(username, password, familyName, givenName, email);
