42d05f7d666e5ad65d7c58575b8831f4767956d6
Vlad <vlad.pasca17@gmail.com>
2024-04-10 03:27:34 +0300

    Lab02

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -1,26 +1,74 @@
 package com.example.myapplication;
 
-import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import androidx.test.rule.ActivityTestRule;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import com.example.myapplication.activity.MainActivity;
+
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class RegistrationInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.myapplication", appContext.getPackageName());
+    public void testRegisterButtonOpensDialog() {
+        // Klicken auf den Registrierungsbutton, um das Dialogfeld anzuzeigen
+        onView(withId(R.id.registerAction)).perform(click());
+
+        // Überprüfen, ob das Dialogfeld angezeigt wird
+        onView(withId(R.id.popupDialog)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testRegisterWithValidCredentials() {
+        // Klicken auf den Registrierungsbutton, um das Dialogfeld anzuzeigen
+        onView(withId(R.id.registerAction)).perform(click());
+
+        // Eingabe gültiger Benutzerdaten im Dialogfeld
+        onView(withId(R.id.Username)).perform(typeText("ValidUser"));
+        onView(withId(R.id.Password)).perform(typeText("ValidPassword123"));
+        onView(withId(R.id.NumeFamilie)).perform(typeText("ValidFamilyName"));
+        onView(withId(R.id.Prenume)).perform(typeText("ValidGivenName"));
+        onView(withId(R.id.Email)).perform(typeText("valid@example.com"));
+
+        // Klicken auf die Registrierungsschaltfläche im Dialogfeld
+        onView(withId(R.id.registerAction)).perform(click());
+
+        // Überprüfen, ob der Benutzer erfolgreich registriert wurde (Anzeige der Hauptaktivität)
+        onView(withText("Bine ați venit!")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testRegisterWithInvalidCredentials() {
+        // Klicken auf den Registrierungsbutton, um das Dialogfeld anzuzeigen
+        onView(withId(R.id.registerAction)).perform(click());
+
+        // Eingabe ungültiger Benutzerdaten im Dialogfeld
+        onView(withId(R.id.Username)).perform(typeText("Short")); // Ungültiger Benutzername
+        onView(withId(R.id.Password)).perform(typeText("Short")); // Ungültiges Passwort
+        onView(withId(R.id.Email)).perform(typeText("invalid")); // Ungültige E-Mail-Adresse
+
+        // Klicken auf die Registrierungsschaltfläche im Dialogfeld
+        onView(withId(R.id.registerAction)).perform(click());
+
+        // Überprüfen, ob Fehlermeldungen angezeigt werden
+        onView(withText("Username too short")).check(matches(isDisplayed()));
+        onView(withText("Password too short")).check(matches(isDisplayed()));
+        onView(withText("Enter a valid email address")).check(matches(isDisplayed()));
     }
 }
--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -14,4 +14,69 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    /***
+     * Teste pentru Lab02 - UVSS
+     * Check UVSS/Lab02/Lab02_BBT_TCs_Form for detailed ECPs and BVAs
+     */
+
+    public boolean isRegistrationValid(String username, String password) {
+        //simulation function
+
+        /*** user pressed the register button
+
+         -dialog popup
+
+         -user enter their first name, last name, email, username & password
+
+         -user presses "submit" */
+
+        return isUsernameValid(username) && isPasswordValid(password);
+    }
+
+    private boolean isUsernameValid(String username) {
+        return username.length() >= 5 && username.length() <= 25 && !username.contains(" ");
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() >= 6 && password.length() <= 25 && !password.contains(" ");
+    }
+
+    @Test
+    public void testValidUsernameAndPassword() {
+        //username: 6 - gultig, password: 6 - gultig
+        assertTrue(isRegistrationValid("VladP7", "123456"));
+    }
+
+    @Test
+    public void testUsernameTooShort() {
+        //username: 4 - nicht gultig, password: 6 - gultig
+        assertFalse(isRegistrationValid("vlad", "123456"));
+    }
+
+    @Test
+    public void testPasswordTooShort() {
+        //username: 25 - gultig, password: 5 - nicht gultig
+        assertFalse(isRegistrationValid("pascavladeugen20021017cjn", "12345"));
+    }
+
+    @Test
+    public void testUsernameTooLong() {
+        //username: 26 - nicht gultig, password: 6 - gultig
+        assertFalse(isRegistrationValid("pascavladeugen20021017cjnp", "123456"));
+    }
+
+    @Test
+    public void testPasswordTooLong() {
+        //username: 25 - gultig, password: 26 - nicht gultig
+        assertFalse(isRegistrationValid("pascavladeugen20021017cjn", "12345678901234567890123456"));
+    }
+
+    @Test
+    public void testInvalidUsernameAndPassword() {
+        //username: 26 - nicht gultig, password: 26 - nicht gultig
+        assertFalse(isRegistrationValid("pascavladeugen20021017cjnp", "12345678901234567890123456"));
+    }
+
+
 }
